cf677af2bb1dc110b3aa7f25d0b915c8895039db
matusca96 <gomesloud@gmail.com>
2019-04-11 18:19:12 -0300

    Atualização

--- webapp/services/ErrorHandler.js
+++ webapp/services/ErrorHandler.js
@@ -4,7 +4,7 @@ sap.ui.define([
 	], function (UI5Object, MessageBox) {
 		"use strict";
 
-		return UI5Object.extend("br.com.idxtecPlanoConta.services.ErrorHandler", {
+		return UI5Object.extend("br.com.idxtecProdutos.services.ErrorHandler", {
 
 			constructor : function (oComponent) {
 				this._oComponent = oComponent;
